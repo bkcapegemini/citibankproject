7fb12f5d24059fb3405331c830be7424bc58ca86
bkcapegemini <chaitanyakrishnacmdr@gmail.com>
2022-09-13 12:01:51 +0530

    new changes in the developer

--- application.java
+++ application.java
@@ -1,4 +1,8 @@
 public class Simple{
 public static void main(String args[]){
 System.out.println("hello javatpoint");
+public void main(String[] args)  
+static void main(String[] args)  
+public void static main(String[] args)  
+abstract public static void main(String[] args)
 }}
